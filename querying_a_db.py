1317c1bf79c116e0fb1c07905b2f434266291c9a
kwonj1234 <kwonj1234@gmail.com>
2020-01-27 22:50:56 -0500

    solved a few more, but not the second to last

--- querying_a_db.py
+++ querying_a_db.py
@@ -10,7 +10,8 @@ with sqlite3.connect("sitemetrics.db") as connection:
     most_page_views = c.fetchall() #Edison Mcintyre, 19937, ME
 
 ##### Who has the least page views? How many do they have and where are they from?
-    c.execute("""SELECT * FROM users WHERE page_views = (SELECT MIN(page_views) FROM users)""")
+    c.execute("""SELECT * FROM users 
+        WHERE page_views = (SELECT MIN(page_views) FROM users)""")
     least_page_views = c.fetchall() #Hattie Ross, 16, MA
 
 ##### Who are the most recent visitors to the site?(at least 3)
@@ -40,15 +41,34 @@ with sqlite3.connect("sitemetrics.db") as connection:
     v_cities = c.fetchall() #Van, Valley View, Victoria, Vega
 
 ##### What are the names and home cities for people searched for the word "drain"?
-    c.execute("""SELECT user.name, user.city FROM users JOIN user_searches ON users.id = user_searches.user_id JOIN search_terms WHERE :word = "drain" """)
-    drain_users = c.fetchall()
-    print(drain_users)
+    c.execute("""SELECT users.name, users.city FROM users 
+        JOIN user_searches ON users.id = user_searches.user_id 
+        JOIN search_terms ON search_terms.id = user_searches.term_id 
+        WHERE word = "drain" """)
+    #read the SQL_JOINS.md honestly I can't believe this worked.
+    drain_users = c.fetchall() 
+    #Nelly Beach - Graford, Penelope Stein - Runaway Bay, Tisha Gill - Bausell and Ellis, Rolando Crowley - Buda
 
 ##### How many times was "trousers" a search term?
+    c.execute("""SELECT COUNT(*) FROM search_terms WHERE word = "trousers" """)
+    num_trousers = c.fetchall() #1
 
 ##### What were the search terms used by visitors who last visited on August 22 2014?
+    c.execute("""SELECT search_terms.word FROM search_terms 
+        JOIN user_searches ON search_terms.id = user_searches.term_id 
+        JOIN users ON users.id = user_searches.user_id 
+        WHERE last_visit = "2014-08-22" """)
+    searchterm_visitors = c.fetchall() #sweet, or, left, word, female, ball
 
 ##### What was the most frequently used search term by people from Idaho?
 
 ##### What is the name of user 391, and what are his search terms?
+    c.execute("""SELECT name FROM users WHERE id = 391""")
+    name391 = c.fetchall() #Stan Alston
+    c.execute("""SELECT search_terms.word FROM search_terms 
+        JOIN user_searches ON search_terms.id = user_searches.term_id 
+        JOIN users ON users.id = user_searches.user_id 
+        WHERE user_id = 391""")
+    searchterms391 = c.fetchall() #ornament, heat, sex, secret, dry
+
 
